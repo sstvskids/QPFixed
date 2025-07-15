d6911bd6dd11ae28a489172969bf378834f767a9
sstvskids <126665568+sstvskids@users.noreply.github.com>
2025-07-15 19:21:21 +1000

    Update fixer.lua
    Added a hook that automatically redirects to the GitHub URL, resulting in no errors when the URL is down.

--- fixer.lua
+++ fixer.lua
@@ -6,6 +6,7 @@ local Players = cloneref(game:GetService('Players'))
 local lplr = Players.LocalPlayer
 
 local old = clonefunction(debug.getupvalue)
+local oldhttp = clonefunction(game.HttpGet)
 hookfunction(debug.getupvalue, function(func, index)
 	if func == require(lplr.PlayerScripts.TS.knit).setup and index == 6 then
 		return old(func, 9)
@@ -14,7 +15,13 @@ hookfunction(debug.getupvalue, function(func, index)
 	return old(func, index)
 end)
 
-print('QP-Vape fixer ran! There is no going back after gng :pray:')
-return loadstring(game:HttpGet("https://qpvape.org"))()
+hookfunction(game.HttpGet, function(url: string)
+    if string.find(url, 'https://qpvape.org') then
+        return oldhttp('https://raw.githubusercontent.com/QP-Offcial/VapeV4ForRoblox/main/NewMainScript.lua')
+    end
+    
+    return oldhttp(url)
+end)
 
+return loadstring(game:HttpGet('https://qpvape.org'))()
 -- ._stav was here, report any issues to me on discord
